d468cc5100fd67fd63fd88bdbf2636c33b81d6aa
yashvanthselvakumar-dotcom <yashvanthselvakumar@gmail.com>
2026-07-07 16:25:40 +0530

    Update SecurityConfig.java

--- src/main/java/com/example/FitLog/Config/SecurityConfig.java
+++ src/main/java/com/example/FitLog/Config/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedOrigins(List.of("https://yashvanthselvakumar.github.io/"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
